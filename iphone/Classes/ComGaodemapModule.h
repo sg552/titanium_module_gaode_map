5282956927fc63d3207881ade8e4a65ef2b4e9d3
Shen Siwei <shensiwei@sina.com>
2016-01-08 17:14:27 +0800

    initial commit

---
+++ iphone/Classes/ComGaodemapModule.h
@@ -0,0 +1,14 @@
+/**
+ * gaodemap
+ *
+ * Created by Siwei
+ * Copyright (c) 2016 Your Company. All rights reserved.
+ */
+
+#import "TiModule.h"
+
+@interface ComGaodemapModule : TiModule
+{
+}
+
+@end
---
+++ iphone/Classes/ComGaodemapModuleAssets.m
@@ -0,0 +1,24 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComGaodemapModuleAssets.h"
+
+extern NSData* filterDataInRange(NSData* thedata, NSRange range);
+
+@implementation ComGaodemapModuleAssets
+
+- (NSData*) moduleAsset
+{
+	
+
+	return nil;
+}
+
+- (NSData*) resolveModuleAsset:(NSString*)path
+{
+	
+
+	return nil;
+}
+
+@end
